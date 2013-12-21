767489182fb6261bac0f7a9bfe6564372ee175a1
Daniel-Xu <xwd.forever@gmail.com>
2013-12-21 17:05:07 -0500

    client side allow insert

--- collections/posts.js
+++ collections/posts.js
@@ -1,3 +1,9 @@
 //notice that there's no var before variable
 //so it will be accessible in the whole app
 Posts = new Meteor.Collection('posts');
+
+Posts.allow({
+    insert: function(userId, doc){
+        return !! userId; 
+    }
+})
