c19f9481c4f74452e16efa09c0ecbaefaa3b7629
Daniel-Xu <xwd.forever@gmail.com>
2013-12-22 16:20:29 -0500

    in colleciton: we define a method, called post,
    in client: we invoke it
    
    notice: user can add post in browser's console, invoking Meteor.call
    but we strict the parameter, remember we use underscore's pick and
    extend

--- client/views/posts/post_submit.js
+++ client/views/posts/post_submit.js
@@ -8,7 +8,13 @@ Template.postSubmit.events({
             message: $(e.target).find('[name=message]').val()
         }
 
-        post._id = Posts.insert(post)
-        Router.go('postPage', post);
+        //the post is the postAttributes in definition of post function
+        
+        Meteor.call('post', post, function(err, id){
+            if(err) 
+                return alert(err.reason)
+
+            Router.go('postPage', {_id: id})
+        })
     }
 })
--- collections/posts.js
+++ collections/posts.js
@@ -2,8 +2,35 @@
 //so it will be accessible in the whole app
 Posts = new Meteor.Collection('posts');
 
-Posts.allow({
-    insert: function(userId, doc){
-        return !! userId; 
+//Posts.allow({
+    //insert: function(userId, doc){
+        //return !! userId; 
+    //}
+//})
+
+
+Meteor.Method({
+    post: function(postAttributes){
+        var user = Meteor.user(), postWithSameLink = Posts.findOne({ url: postAttributes.url})
+
+        if(!user)
+            throw new Meteor.Error(401, " You need to log in!")
+
+        if(!postAttributes.title)
+            throw new Meteor.Error(422, " Title counld not be empty!")
+
+        if(postAttributes.url && postWithSameLink)
+            throw new Meteor.Error(302. " The url are already be post")
+        
+
+        var post = _.extend(_.pick(postAttributes, 'url', 'title', 'message'), {
+            usersId: user._id, 
+            author: user.username,
+            submitted: new Date().getTime()
+        })
+    
+        var postId = Posts.insert(post)
+        return postId
     }
+
 })
