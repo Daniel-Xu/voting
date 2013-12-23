9be6b38a22c4ca2bff8fbc8fa166be4096b55efc
Daniel-Xu <xwd.forever@gmail.com>
2013-12-23 16:12:02 -0500

    fixture data

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,21 +1,55 @@
+// Fixture data 
 if (Posts.find().count() === 0) {
-    Posts.insert({
-        title: "Introducing Voting", 
-        author: "Daniel", 
-        url: "http://www.google.com"
+  var now = new Date().getTime();
 
-    });
+  // create two users
+  var tomId = Meteor.users.insert({
+    profile: { name: 'Tom Coleman' }
+  });
+  var tom = Meteor.users.findOne(tomId);
+  var sachaId = Meteor.users.insert({
+    profile: { name: 'Sacha Greif' }
+  });
+  var sacha = Meteor.users.findOne(sachaId);
 
-    Posts.insert({
-        title: "Meteor", 
-        author: "Tome Coleman", 
-        url: "http://www.meteor.com"
+  var telescopeId = Posts.insert({
+    title: 'Introducing Telescope',
+    userId: sacha._id,
+    author: sacha.profile.name,
+    url: 'http://sachagreif.com/introducing-telescope/',
+    submitted: now - 7 * 3600 * 1000
+  });
 
-    });
-    Posts.insert({
-        title: "Discover Meteor", 
-        author: "Tom Coleman", 
-        url: "http://www.baidu.com"
-    });
+  Comments.insert({
+    postId: telescopeId,
+    userId: tom._id,
+    author: tom.profile.name,
+    submitted: now - 5 * 3600 * 1000,
+    body: 'Interesting project Sacha, can I get involved?'
+  });
+
+  Comments.insert({
+    postId: telescopeId,
+    userId: sacha._id,
+    author: sacha.profile.name,
+    submitted: now - 3 * 3600 * 1000,
+    body: 'You sure can Tom!'
+  });
+
+  Posts.insert({
+    title: 'Meteor',
+    userId: tom._id,
+    author: tom.profile.name,
+    url: 'http://meteor.com',
+    submitted: now - 10 * 3600 * 1000
+  });
+
+  Posts.insert({
+    title: 'The Meteor Book',
+    userId: tom._id,
+    author: tom.profile.name,
+    url: 'http://themeteorbook.com',
+    submitted: now - 12 * 3600 * 1000
+  });
 }
 
