9c2899e249f5e83e8f889336d05bb1a2c21301c1
Daniel-Xu <xwd.forever@gmail.com>
2013-12-23 11:03:33 -0500

    The most important thing in this commit is: context.
    when you use go to a router, you should notice the data context.
    
    and when you use pathFor, it provide the "this" context for your router
    to use

--- client/views/posts/post_item.js
+++ client/views/posts/post_item.js
@@ -3,5 +3,11 @@ Template.postItem.helpers({
         var a = document.createElement('a') 
         a.href = this.url;
         return a.hostname;
+    }, 
+
+    ownPost: function(){
+        // you should notice: the userId is we add when we create post properties
+        // in post.js
+        return this.userId == Meteor.userId()
     }
 });
--- collections/posts.js
+++ collections/posts.js
@@ -24,7 +24,7 @@ Meteor.methods({
         
 
         var post = _.extend(_.pick(postAttributes, 'url', 'title', 'message'), {
-            usersId: user._id, 
+            userId: user._id, 
             author: user.username,
             submitted: new Date().getTime()
         })
