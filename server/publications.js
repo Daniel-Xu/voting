86d0b557032f26f6938e3983f41d1d03e7aca266
Daniel-Xu <xwd.forever@gmail.com>
2013-12-25 10:53:48 -0500

    Should figure out "this" in router

--- server/publications.js
+++ server/publications.js
@@ -3,6 +3,9 @@ Meteor.publish('posts', function(options){
     return Posts.find({}, options)
 })
 
+Meteor.publish('singlePost', function(id){
+    return id && Posts.find(id)
+})
 //the callback called on the server each time a client subscribes
 //and the parameter is the same with subscribing process passes
 Meteor.publish('comments', function(postId){
