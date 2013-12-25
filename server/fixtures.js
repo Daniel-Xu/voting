fae124f1d3257e7b6a76f440fe8043a6a86dbf49
Daniel-Xu <xwd.forever@gmail.com>
2013-12-25 08:23:16 -0500

    add more post data

--- server/fixtures.js
+++ server/fixtures.js
@@ -54,5 +54,16 @@ if (Posts.find().count() === 0) {
     submitted: now - 12 * 3600 * 1000, 
     commentsCount: 0
   });
+
+  for (var i=0; i < 10; i++) {
+      Posts.insert({
+        title: "Test Post "+ i,
+        userId: tom._id, 
+        author: tom.profile.name,
+        url: 'http://www.google.com?q=test-'+i,
+        submitted: now - i * 3600 * 1000, 
+        commentsCount: 0
+      })
+  };
 }
 
