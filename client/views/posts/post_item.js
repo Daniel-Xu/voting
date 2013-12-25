d9c5ecdfc8812a4df2d77f565c0d0e42c3102256
Daniel-Xu <xwd.forever@gmail.com>
2013-12-25 16:55:51 -0500

    find and findOne

--- client/views/posts/post_item.js
+++ client/views/posts/post_item.js
@@ -14,4 +14,21 @@ Template.postItem.helpers({
     //commentsCount: function(){
         //return Comments.find({postId: this._id}).count()
     //}
+    upvotedClass: function(){
+        var userId = Meteor.userId()
+
+        if ( userId && !_.include(this.upvoters, userId)) {
+            return 'btn-primary upvotable'
+        } else {
+            return 'disabled'
+        }
+    }
 });
+
+
+Template.postItem.events({
+    'click .upvote': function(e){
+        e.preventDefault()
+        Meteor.call('upvote', this._id)
+    }
+})
